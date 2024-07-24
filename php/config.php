67c133f271fcc62fd4ba617628b76d3e22f6a986
v45r26 <161456810+v45r26@users.noreply.github.com>
2024-07-24 12:24:54 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$_server = 'fdb1029.awardspace.net';
-$_username = '3523635_chtdb';
-$_password = 'y^A.GLk@19J{44w(';
-$_db = '3523635_chtdb';
+$_server = 'localhost';
+$_username = 'root';
+$_password = '';
+$_db = 'chtdb';
 
 $conn = mysqli_connect($_server, $_username, $_password, $_db);
 if (!$conn)
